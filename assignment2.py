1c67de06331321b86c488b4a218592b17f837347
tirumal~ <Pathipatiturumala@gmail.com>
2026-06-24 12:48:04 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,13 @@
+list=[1,2,3,4,5]
+print(list)
+list.append(6)
+print(list)
+list.insert(0,0)
+print(list)
+list.remove(2)
+print(list)
+list.sort()
+print(list)
+print(list[2])
+copy=list[:]
+print(copy)
